e015036f8561674bc6cc1e4ef5ea5dc67214a27f
tose2125 <tose2125@tose2125.net>
2016-11-17 11:56:35 +0900

    最低限の機能を実装。

---
+++ UtaskSyllabusScraper/MainForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace UtaskSyllabusScraper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileDropPanel = new System.Windows.Forms.Panel();
+            this.mainFormLabel = new System.Windows.Forms.Label();
+            this.directorySelectDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.fileDropPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileDropPanel
+            // 
+            this.fileDropPanel.AllowDrop = true;
+            this.fileDropPanel.Controls.Add(this.mainFormLabel);
+            this.fileDropPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fileDropPanel.Location = new System.Drawing.Point(0, 0);
+            this.fileDropPanel.Margin = new System.Windows.Forms.Padding(10);
+            this.fileDropPanel.Name = "fileDropPanel";
+            this.fileDropPanel.Padding = new System.Windows.Forms.Padding(10);
+            this.fileDropPanel.Size = new System.Drawing.Size(284, 261);
+            this.fileDropPanel.TabIndex = 0;
+            this.fileDropPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.fileDropPanel_DragDrop);
+            this.fileDropPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.fileDropPanel_DragEnter);
+            // 
+            // mainFormLabel
+            // 
+            this.mainFormLabel.AutoSize = true;
+            this.mainFormLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mainFormLabel.Location = new System.Drawing.Point(10, 10);
+            this.mainFormLabel.Name = "mainFormLabel";
+            this.mainFormLabel.Size = new System.Drawing.Size(107, 12);
+            this.mainFormLabel.TabIndex = 0;
+            this.mainFormLabel.Text = "ここにファイルをドロップ";
+            // 
+            // directorySelectDialog
+            // 
+            this.directorySelectDialog.Description = "HTMLファイルごとにJSONファイルを出力します。";
+            this.directorySelectDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "csv";
+            this.saveFileDialog.Filter = "CSV (コンマ区切り)|*.csv|すべてのファイル|*.*";
+            this.saveFileDialog.Title = "全HTMLファイルからCSVファイルを出力します。";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.fileDropPanel);
+            this.Name = "MainForm";
+            this.Text = "UtaskSyllabusScraper";
+            this.fileDropPanel.ResumeLayout(false);
+            this.fileDropPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel fileDropPanel;
+        private System.Windows.Forms.Label mainFormLabel;
+        private System.Windows.Forms.FolderBrowserDialog directorySelectDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ UtaskSyllabusScraper/MainForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace UtaskSyllabusScraper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //ファイルをドラッグした時に、ドロップ可能であることを表示します。
+        private void fileDropPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        //ファイルをドロップした時に、ファイルの処理を始めます。
+        private void fileDropPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                List<string> fileList = ((string[])e.Data.GetData(DataFormats.FileDrop)).ToList<string>();
+                if (fileList.Count > 0 && saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //全HTMLファイルからCSVファイルを出力します。
+                    string outputPath = saveFileDialog.FileName;
+                    string outputCsv = "";
+                    fileList.ForEach(filePath =>
+                    {
+                        HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
+                        html.Load(filePath, Encoding.UTF8);
+                        HtmlNodeCollection dataCellList = html.DocumentNode.SelectNodes("//table[@class=\"syllabus-normal\"]//td");
+                        if (dataCellList == null)
+                        {
+                            Console.Write("xpath探索結果がnull " + filePath);
+                            return;
+                        }
+                        Dictionary<string, string> outputList = new Dictionary<string, string>(dataCellList.Count);
+                        string outputText = "";
+                        for (int i = 0; i < dataCellList.Count; i++)
+                        {
+                            if (dataCellList[i].HasChildNodes && dataCellList[i].ChildNodes[0].Name == "a")
+                            {
+                                dataCellList[i] = dataCellList[i].ChildNodes[0];
+                            }
+                            outputText = outputText + "\"" + dataCellList[i].InnerHtml.Replace("\"", "\\\"").Replace("\r", "\\r").Replace("\n", "\\n").Replace("<br>", "\\n").Replace("\\n\\n", "\\n") + "\",";
+                        }
+                        outputCsv = outputCsv + outputText.Substring(0, outputText.Length - 1) + Environment.NewLine;
+                    });
+                    try
+                    {
+                        StreamWriter streamWriter = new StreamWriter(outputPath, false, Encoding.UTF8);
+                        streamWriter.Write(outputCsv);
+                        streamWriter.Close();
+                    }
+                    catch (IOException error)
+                    {
+                        Console.Write(error.Message);
+                    }
+                }
+                if (fileList.Count > 0 && directorySelectDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //HTMLファイルごとにJSONファイルを出力します。
+                    string outputPath = directorySelectDialog.SelectedPath;
+                    if (outputPath.Substring(outputPath.Length - 1) != "" + Path.DirectorySeparatorChar)
+                    {
+                        outputPath = outputPath + Path.DirectorySeparatorChar;
+                    }
+                    fileList.ForEach(filePath =>
+                    {
+                        HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
+                        html.Load(filePath, Encoding.UTF8);
+                        HtmlNodeCollection titleCellList = html.DocumentNode.SelectNodes("//table[@class=\"syllabus-normal\"]//th");
+                        HtmlNodeCollection dataCellList = html.DocumentNode.SelectNodes("//table[@class=\"syllabus-normal\"]//td");
+                        if (titleCellList == null || dataCellList == null)
+                        {
+                            Console.Write("xpath探索結果がnull " + filePath);
+                            return;
+                        }
+                        if (titleCellList.Count != dataCellList.Count)
+                        {
+                            Console.Write("xpath探索結果の数一致せず " + filePath);
+                            return;
+                        }
+                        Dictionary<string, string> outputList = new Dictionary<string, string>(dataCellList.Count);
+                        for (int i = 0; i < dataCellList.Count; i++)
+                        {
+                            if (dataCellList[i].HasChildNodes && dataCellList[i].ChildNodes[0].Name == "a")
+                            {
+                                dataCellList[i] = dataCellList[i].ChildNodes[0];
+                            }
+                            outputList.Add(titleCellList[i].InnerText, dataCellList[i].InnerHtml.Replace("\r", "").Replace("\n", "").Replace("<br>", "\n"));
+                        }
+                        JavaScriptSerializer serializer = new JavaScriptSerializer();
+                        string outputJson = serializer.Serialize(outputList);
+                        try
+                        {
+                            FileInfo fileInfo = new FileInfo(filePath);
+                            string savePath = outputPath + fileInfo.Name.Replace(fileInfo.Extension, ".json");
+                            if (new FileInfo(savePath).Exists)
+                            {
+                                int count = 1;
+                                string savePathFront = savePath.Substring(0, savePath.Length - 5);
+                                while (new FileInfo(savePath).Exists)
+                                {
+                                    savePath = savePathFront + " (" + count + ").json";
+                                    count++;
+                                }
+                            }
+                            StreamWriter streamWriter = new StreamWriter(savePath, false, Encoding.UTF8);
+                            streamWriter.Write(outputJson);
+                            streamWriter.Close();
+                        }
+                        catch (IOException error)
+                        {
+                            Console.Write(error.Message);
+                        }
+                    });
+                }
+            }
+        }
+
+    }
+}
